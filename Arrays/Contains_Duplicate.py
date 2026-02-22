0be177de91416b8dc7c56969e0ecac60bd04c786
Raza Aziz <razaaziz9191@gmail.com>
2026-02-22 20:36:42 +0500

    Solution for Contains Duplicate

--- Arrays/Contains_Duplicate.py
+++ Arrays/Contains_Duplicate.py
@@ -12,3 +12,11 @@ class Solution(object):
             else:
                 seen.add(n)
         return False
+
+
+# Explanation:
+# - Again, use hashing (Hash set)
+#   1. Iterate through the array
+#   2. Check if num is in set
+#   2.1 IF exists: return True as already present before in array
+#   2.2 ELSE: Just add in set

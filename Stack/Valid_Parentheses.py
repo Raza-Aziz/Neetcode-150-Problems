1f513246080043a94d58150901395809888ac662
Raza Aziz <razaaziz9191@gmail.com>
2026-04-08 17:52:33 +0500

    Solution for Problem 20. Valid Parentheses

---
+++ Stack/Valid_Parentheses.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+        closed_to_open = {")": "(", "}": "{", "]": "["}
+
+        for c in s:
+            if c in closed_to_open:
+                if stack and stack[-1] == closed_to_open[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
+
+# Implementation Steps:
+# 1. Make a stack (list)
+# 2. Create a hashmap with closing brackets as keys & opening as values
+# 3. Run a loop over string
+#   3.1 First check if the bracket is in hashmap (meaning if it's a closing bracket)?
+#       3.1.1 If yes, then check if stack is NOT empty (meaning it has an opening bracket, doesn't matter whose)
+#       3.1.2 AND if the last item of stack is opening bracket of that closing bracket (check from hashmap)
+#           o If yes, then simply remove that last item (stack.pop)
+#           o Else, return False
+#   3.2 Else:
+#       o Push the bracket in stack
+# 4. Return true if stack is empty (meaning brackets in string were correctly placed)

2c336851f648543a9d6a029efe0d485a319394cb
Raza Aziz <razaaziz9191@gmail.com>
2026-04-14 00:03:42 +0500

    refactor: Change c to bracket for clarity

--- Stack/Valid_Parentheses.py
+++ Stack/Valid_Parentheses.py
@@ -8,14 +8,14 @@ class Solution(object):
         stack = []
         closed_to_open = {")": "(", "}": "{", "]": "["}
 
-        for c in s:
-            if c in closed_to_open:
-                if stack and stack[-1] == closed_to_open[c]:
+        for bracket in s:
+            if bracket in closed_to_open:
+                if stack and stack[-1] == closed_to_open[bracket]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(c)
+                stack.append(bracket)
 
         return True if not stack else False
 

cf14bf9054af23b268f35314e25f0992b966322c
Raza Aziz <razaaziz9191@gmail.com>
2026-05-03 02:05:25 +0500

    Solution for Problem 509. Fibonacci Number using 1. Recursion 2. Space Optimization (using only variables / Two Pointer solution)    3. Recursion + Memoization (DP)

--- Fibonacci_series.py
+++ Fibonacci_series.py
@@ -1,6 +1,38 @@
 class Solution:
     def fib(self, n: int) -> int:
+        # if n == 0 or n == 1:
+        #     return n
+
+        # return self.fib(n - 1) + self.fib(n - 2)
+
+        # DP + Tabulation + Space Optimization
+        prev_1 = 0
+        prev_2 = 1
+
+        if n == 0:
+            return 0
+
+        for i in range(2, n + 1):
+            curr = prev_1 + prev_2
+
+            prev_1 = prev_2
+            prev_2 = curr
+
+        return prev_2
+
+    # Using Memoization + Recursion
+    def fib_recursive(self, n: int, fibd={}) -> int:
+
         if n == 0 or n == 1:
             return n
-        
-        return self.fib(n - 1) + self.fib(n - 2)
+
+        if n in fibd:
+            return fibd[n]
+
+        n_1 = self.fib(n - 1, fibd)
+        n_2 = self.fib(n - 2, fibd)
+        result = n_1 + n_2
+
+        fibd[n] = result
+
+        return result
